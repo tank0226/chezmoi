c33aa837eb108b0bf19052e8a8e096648a6b651e
Tom Payne <twpayne@gmail.com>
2020-12-30 00:16:16 +0100

    snapshot

--- chezmoi2/internal/chezmoi/targetstateentry.go
+++ chezmoi2/internal/chezmoi/targetstateentry.go
@@ -1,7 +1,5 @@
 package chezmoi
 
-// FIXME I don't think we need to use lazyContents here, except the SHA256 stuff is useful
-
 import (
 	"bytes"
 	"encoding/hex"
@@ -329,7 +327,6 @@ func (t *TargetStateScript) EntryState() (*EntryState, error) {
 // Equal returns true if actualStateEntry matches t.
 func (t *TargetStateScript) Equal(actualStateEntry ActualStateEntry, umask os.FileMode) (bool, error) {
 	// Scripts are independent of the actual state.
-	// FIXME maybe the destination state should store the sha256 sums of executed scripts
 	return true, nil
 }
 
